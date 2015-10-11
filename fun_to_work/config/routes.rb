644a992d87cdfe34aa7da7ae8643c262d7310a26
Gabriel Arreola <gabrielarreola@airbears2-10-142-38-182.AirBears2.1918.Berkeley.EDU>
2015-10-10 19:32:07 -0700

    well, I feel like I've come a long way. This calhacks wasn't exactly what I expected, but you know what, I think I've really learned something. And isn't that what this is all about. Oh, we linked the paths and stuff.

--- fun_to_work/config/routes.rb
+++ fun_to_work/config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/about'
+  root             'static_pages#home'
+  get 'about'   => 'static_pages#about'
 
   resources :microposts
 
   resources :users
-  root 'static_pages#home'
 
   get 'welcome/index'
 
