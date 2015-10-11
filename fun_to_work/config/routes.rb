caf2b63b00c3c911b15bf5d1ef9b29c5f2d579e1
Gabriel Arreola <gabrielarreola@airbears2-10-142-38-182.AirBears2.1918.Berkeley.EDU>
2015-10-10 19:41:36 -0700

    sign ingit add .

--- fun_to_work/config/routes.rb
+++ fun_to_work/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   root             'static_pages#home'
   get 'about'   => 'static_pages#about'
+  get 'signup'  => 'users#new'
 
   resources :microposts
 
